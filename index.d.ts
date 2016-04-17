c13a3d24ba1bc7ec4a16fd1f8370bfe7cf83d9d5
Mike Wilcox <mwilcox56@gmail.com>
2016-04-17 17:59:42 -0400

    update type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,67 @@
+export interface Inning {
+    num: string;
+    bottom: Bottom;
+    top: Top;
+}
+
+export interface Bottom {
+    atbat: AtBat[];
+}
+
+export interface Top {
+    action: Action;
+    atbat: AtBat[];
+}
+
+export interface Action {
+    home_team_runs: string;
+    des: string;
+    b: string;
+    player: string;
+    event_num: string;
+    o: string;
+    des_es: string;
+    event_es: string;
+    s: string;
+    event: string;
+    tfs: string;
+    away_team_runs: string;
+    tfs_zulu: string;
+    putch: string;
+}
+
+export interface Pitch {
+    start_speed: string;
+    des: string;
+    sv_id: string;
+    pitch_type: string;
+    type: string;
+    des_es: string;
+}
+
+export interface AtBat {
+    home_team_runs: string;
+    des: string;
+    b: string;
+    event_num: string;
+    b3: string;
+    pitcher: string;
+    b2: string;
+    o: string;
+    batter: string;
+    des_es: string;
+    event_es: string;
+    num: string;
+    start_tfs_zulu: string;
+    s: string;
+    event: string;
+    b1: string;
+    play_guid: string;
+    pitch: Pitch[];
+    away_team_runs: string;
+    start_tfs: string;
+}
+
 export interface Batting {
     hr: string;
     d: string;
@@ -149,7 +213,14 @@ export interface BoxScore {
     home_fname: string;
 }
 
+export interface Events {
+    inning: Inning[];
+    atBat: { pid: string; };
+    deck: { pid: string; };
+    hole: { pid: string; };
+}
+
 export declare const gameIds: (date: Date) => Axios.IPromise<string[]>;
 export declare const getGameBoxScoreById: (id: string) => Promise<BoxScore>;
-export declare const getGameEventsById: (id: string) => Promise<any>;
+export declare const getGameEventsById: (id: string) => Promise<Events>;
 
